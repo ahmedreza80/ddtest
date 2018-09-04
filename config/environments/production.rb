a918e5f4a82a1e9751aec7d297c1d26731b91c73
ahmedreza80 <mdahmed@shiftind.com>
2018-09-05 00:22:32 +0530

    hdjjff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,14 +28,14 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Specifies the header that your server uses for sending files.
+  # Specifies the header that your server uses for sending files.000000000000000000
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
