0769584beef79e3238b2a3b5f487d4b09bbef85c
ahmedreza80 <mdahmed@shiftind.com>
2018-09-05 15:14:05 +0530

    ddg

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,4 +1,4 @@
-require 'elasticsearch/model'
+
 
 class Bench < ApplicationRecord
 	mount_uploader :image, ImageUploader
@@ -6,7 +6,5 @@ class Bench < ApplicationRecord
 	mount_uploader :previewt, PreviewtUploader
 	mount_uploader :previewth, PreviewthUploader
 	validates :description, presence: true, length: { maximum: 600 }
-	include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
 end
-Bench.import(force: true)
+
