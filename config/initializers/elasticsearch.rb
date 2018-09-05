ed926bf208d0c34f0c57b423e29b19bca973a48c
ahmedreza80 <mdahmed@shiftind.com>
2018-09-06 01:26:31 +0530

    ghfj

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,6 @@
+if Rails.env == 'production'
+  url = "https://sxn7d7ni9m:7dr7ldjewi@holly-6981163.us-east-1.bonsaisearch.net"
+  transport_options = { request: { timeout: 250 } }
+  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+  Searchkick.client = Elasticsearch::Client.new(options)
+end
