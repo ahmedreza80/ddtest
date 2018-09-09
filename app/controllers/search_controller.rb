ab2788ddd990552fb4e3b11962d4f13a8d10d38f
ahmedreza80 <mdahmed@shiftind.com>
2018-09-09 23:44:15 +0530

    gfjgjf

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,13 @@
 class SearchController < ApplicationController
-def search
+    def search
   if params[:q].nil?
     @chairs = []
+    @benches = []
+
   else
     @chairs = Chair.search params[:q]
+    @benches = Bench.search params[:q]
+
   end
 end
 end
--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,8 +1,11 @@
+require 'elasticsearch/model'
 class Bench < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	mount_uploader :previewo, PreviewoUploader
 	mount_uploader :previewt, PreviewtUploader
 	mount_uploader :previewth, PreviewthUploader
 	validates :description, presence: true, length: { maximum: 600 }
+	include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
-
+Bench.import force: true
